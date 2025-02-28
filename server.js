ef3037d72b801b491f7c2e15aebdb78c0edc93ae
Regina Vivanco Lemus <revilem1331@gmail.com>
2025-02-28 17:32:59 -0600

    creamos rutas y volvimos los archivos de public estaticos

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+
 const app = express();
+
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('¡Hola, mundo desde Express!');
+app.use(express.static("public"));
+
+app.get('/inicio', (req, res) => {
+    res.send('¡Hola, mundo desde la pagina de inicio');
+});
+
+app.get('/paginaPrincipal', (req, res) => {
+    res.send('¡Hola, mundo desde la pagina principal!');
 });
 
 app.listen(PORT, () => {
